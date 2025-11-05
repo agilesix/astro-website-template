fb5102eeb1f181c5e642cec21955b0a625ef779e
widal001 <billy.daly@agile6.com>
2025-11-04 23:25:19 -0500

    refactor: Reorganizes components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+import { fileURLToPath } from "url";
+import { resolve } from "path";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": resolve(fileURLToPath(new URL("./src", import.meta.url))),
+      },
+    },
   },
 
-  integrations: [mdx()]
+  integrations: [mdx()],
 });
