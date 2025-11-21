2d2c71323c8f082da11ecda799d79eb291472a93
Billy Daly <billy.daly@agile6.com>
2025-11-20 21:56:55 -0500

    Set up collections (#1)
    * feat: Adds sample collections
    * feat: Adds collections to index pages
    * refactor: Updates collections to use new components
    - Post
    - PostCollection

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,42 @@ const pages = defineCollection({
   }),
 });
 
+const blog = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.string(),
+    author: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+const news = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.string(),
+    author: z.string().optional(),
+  }),
+});
+
+const caseStudies = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.string(),
+    client: z.string().optional(),
+    industry: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
 export const collections = {
   pages,
+  blog,
+  news,
+  "case-studies": caseStudies,
 };
